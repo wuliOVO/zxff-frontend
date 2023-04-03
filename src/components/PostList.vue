a77d8945a4787b26c94ae9f7d10de5622373b810
wuliOVO <1795814340@qq.com>
2023-04-03 15:37:58 +0800

    FusionFinder frontend

---
+++ src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-list item-layout="horizontal" :data-source="props.postList">
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a-list-item-meta :description="item.content">
+          <template #title>
+            <a href="https://www.antdv.com/">{{ item.title }}</a>
+          </template>
+          <template #avatar>
+            <a-avatar :src="ikun" />
+          </template>
+        </a-list-item-meta>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+
+<script setup lang="ts">
+import ikun from "../assets/ikun.png";
+import { withDefaults, defineProps } from "vue";
+
+interface Props {
+  postList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  postList: () => [],
+});
+</script>
+
+<style scoped>
+.ikun {
+  width: 200px;
+}
+</style>
